5e7d19cc8a6a6ee9e9d0d4db3c889f3f8cafdfff
rei kasemi <reikasemi2001@gmail.com>
2024-05-06 15:24:57 +0300

    Symbol , Bot , Strategy

---
+++ app/Models/Bot.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Bot extends Model
+{
+    use HasFactory;
+    
+    protected $fillable = ['name', 'user_id', 'exchange_connector_id', 'strategy_id', 'symbol'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function exchangeConnector()
+    {
+        return $this->belongsTo(ExchangeConnector::class);
+    }
+
+    public function strategy()
+    {
+        return $this->belongsTo(Strategy::class);
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\TradingSymbol;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,11 +13,20 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        $symbols = [
+            ['symbol' => 'BTC-PERPETUAL'],
+            ['symbol' => 'ETH-PERPETUAL'],
+            ['symbol' => 'XRP-PERPETUAL'],
+            ['symbol' => 'LTC-PERPETUAL'],
+            ['symbol' => 'ADA-PERPETUAL'],
+            ['symbol' => 'LINK-PERPETUAL'],
+            ['symbol' => 'BCH-PERPETUAL'],
+            ['symbol' => 'DOT-PERPETUAL'],
+            ['symbol' => 'UNI-PERPETUAL'],
+        ];
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        foreach ($symbols as $symbol) {
+            TradingSymbol::create($symbol);
+        }
     }
 }
